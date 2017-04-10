4d0bea2bce085c53c7ebd2180c9b337acf8f1179
Dmitri Golubentsev <dmitrig@compatibl.com>
2017-04-10 12:53:29 +0700

    initial commit

--- esp8266-sous-vide.ino
+++ esp8266-sous-vide.ino
@@ -125,13 +125,6 @@ void loadSettings() {
 
 float printTemperature(DeviceAddress deviceAddress)
 {
-  // method 1 - slower
-  //Serial.print("Temp C: ");
-  //Serial.print(sensors.getTempC(deviceAddress));
-  //Serial.print(" Temp F: ");
-  //Serial.print(sensors.getTempF(deviceAddress)); // Makes a second call to getTempC and then converts to Fahrenheit
-
-  // method 2 - faster
   float tempC = sensors.getTempC(deviceAddress);
   Serial.print("Temp C: ");
   Serial.print(tempC);
